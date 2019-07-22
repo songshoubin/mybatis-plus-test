96f1d248189d6189344902b5fb73f03052d9a766
songshoubin <38982928+songshoubin@users.noreply.github.com>
2019-07-22 17:48:47 +0800

    初步完成

--- src/main/java/com/xiaodu/mybatisplustest/web/ElaJobController.java
+++ src/main/java/com/xiaodu/mybatisplustest/web/ElaJobController.java
@@ -1,6 +1,12 @@
 package com.xiaodu.mybatisplustest.web;
 
 
+import com.xiaodu.mybatisplustest.entity.ElaJob;
+import com.xiaodu.mybatisplustest.service.IElaJobService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -16,5 +22,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/elaJob")
 public class ElaJobController {
+    @Autowired
+    private IElaJobService elaJobService;
 
+    @GetMapping("/getjob")
+    public ResponseEntity getjob(){
+        ElaJob elaJob = elaJobService.selectById(1);
+        return new ResponseEntity(elaJob, HttpStatus.OK);
+
+    }
 }
--- src/test/java/com/xiaodu/mybatisplustest/MybatisPlusTestApplicationTests.java
+++ src/test/java/com/xiaodu/mybatisplustest/MybatisPlusTestApplicationTests.java
@@ -1,16 +1,25 @@
 package com.xiaodu.mybatisplustest;
 
+import com.xiaodu.mybatisplustest.entity.ElaJob;
+import com.xiaodu.mybatisplustest.mapper.ElaJobDao;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@MapperScan("com.xiaodu.mybatisplustest.mapper")
 public class MybatisPlusTestApplicationTests {
 
+    @Autowired
+    ElaJobDao elaJobDao;
     @Test
     public void contextLoads() {
+        ElaJob elaJob = elaJobDao.selectById(1);
+        System.out.println(elaJob.toString());
     }
 
 }
